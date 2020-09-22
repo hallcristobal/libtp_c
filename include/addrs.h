f7b47995fc499f640a01aeb0d91b75d873e0b2da
kipcode66 <egamer1010@gmail.com>
2020-09-22 05:51:09 -0400

    hotfix unwanted character

--- include/addrs.h
+++ include/addrs.h
@@ -37,7 +37,7 @@
 #define tp_mPadStatus_addr 0x804343F0
 #define tp_mPadSticks_addr 0x804343F2
 #define tp_mPadTriggers_addr 0x804343F6
-#define tp_mPadAStick_addr 0x804344E0¸
+#define tp_mPadAStick_addr 0x804344E0
 #define tp_mPadACStick_addr 0x80434520
 #define tp_mPadATriggers_addr 0x80434430
 
